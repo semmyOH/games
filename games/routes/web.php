883384f41b44f8b8d656281334ab033c282bf729
semmyOH <boysandreas3@gmail.com>
2026-09-18 12:54:38 +0200

    Debugbar package geïnstalleerd + Breeze authenticatie geïnstalleerd

--- games/routes/web.php
+++ games/routes/web.php
@@ -1,15 +1,29 @@
 <?php
 
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('games', [App\Http\Controllers\GameController::class, 'index']);
-Route::get('games/create', [App\Http\Controllers\GameController::class, 'create']);
-Route::post('games/store', [App\Http\Controllers\GameController::class, 'store']);
-Route::get('games/show/{id}', [App\Http\Controllers\GameController::class, 'showGame']);
-Route::get('games/edit/{id}', [App\Http\Controllers\GameController::class, 'edit']);
-Route::post('games/update/{id}', [App\Http\Controllers\GameController::class, 'update']);
-Route::delete('games/delete/{id}', [App\Http\Controllers\GameController::class, 'destroy']);
+Route::get('games', [GameController::class, 'index']);
+Route::get('games/create', [GameController::class, 'create']);
+Route::post('games/store', [GameController::class, 'store']);
+Route::get('games/show/{id}', [GameController::class, 'showGame']);
+Route::get('games/edit/{id}', [GameController::class, 'edit']);
+Route::post('games/update/{id}', [GameController::class, 'update']);
+Route::delete('games/delete/{id}', [GameController::class, 'destroy']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
