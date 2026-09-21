ea06a14c58816a964d90f182c415ca2d9067a5d4
semmyOH <boysandreas3@gmail.com>
2026-09-21 14:16:21 +0200

    Middleware auth toegepast

---
+++ games/resources/views/geheim.blade.php
@@ -0,0 +1,2 @@
+<h1>Geheime pagina</h1>
+<p>Dit is een geheime pagina die alleen toegankelijk is voor ingelogde gebruikers.</p>
--- games/routes/web.php
+++ games/routes/web.php
@@ -26,4 +26,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/geheim', function () {
+    return view('geheim');
+})->middleware('auth');
+
 require __DIR__.'/auth.php';
